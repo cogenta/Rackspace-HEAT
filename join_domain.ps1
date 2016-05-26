dbebcd7e04ad947f59bc29f0a8e7a5ee01260735
tommymonk <monk.tommy@gmail.com>
2016-05-26 19:28:11 +0100

    Add files via upload

---
+++ join_domain.ps1
@@ -0,0 +1,14 @@
+$domain$domain=$args[0]
+$oupath=$args[1]
+$credential_username=$args[2]
+$credential_password=$args[3]
+
+# Set DNS Servers
+Set-DnsClientServerAddress -InterfaceAlias "RC-FW-*" -ServerAddresses ("172.30.1.7","172.30.1.8")
+
+# Create credentials
+$secpasswd = ConvertTo-SecureString "$credential_password" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$credential_username", $secpasswd)
+
+# Add to domain
+Add-Computer -DomainName $domain -OUPath "$oupath" -Credential $credential -Restart
